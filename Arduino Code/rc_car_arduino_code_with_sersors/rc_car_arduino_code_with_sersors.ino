79b19d4ce311a4189555954bed95fa9e738a820b
martog <martin.holy.grigorov@gmail.com>
2015-12-12 12:56:54 +0200

    Light sensor added

--- Arduino Code/rc_car_arduino_code_with_sersors/rc_car_arduino_code_with_sersors.ino
+++ Arduino Code/rc_car_arduino_code_with_sersors/rc_car_arduino_code_with_sersors.ino
@@ -1,11 +1,12 @@
-
-//"RBL_nRF8001.h/spi.h/boards.h" is needed in every new project
+#include <BH1750FVI.h>
+#include <Wire.h>
 #include <RBL_services.h>
 #include <SPI.h>
 #include <EEPROM.h>
 #include <boards.h>
 #include <RBL_nRF8001.h>
 
+BH1750FVI LightSensor;
 
 #define forward 3  // Pin 3 - Forward
 #define backward 2 	// Pin 2 - Backward
@@ -27,6 +28,11 @@ void setup()
   ble_set_name("My RC Car"); // The name have to be under 10 letters
 
   Serial.begin(9600);
+  LightSensor.begin();
+
+  LightSensor.SetAddress(Device_Address_H);//Address 0x5C
+  LightSensor.SetMode(Continuous_H_resolution_Mode);
+   
   pinMode(echoPin,INPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(forward, OUTPUT);
@@ -39,6 +45,8 @@ void setup()
 
 unsigned char val;
 unsigned int loop_count = 0;
+int light_sen;
+
 void go_forward() {
   digitalWrite(forward, HIGH);
 }
@@ -71,17 +79,28 @@ void stop_backward() {
   digitalWrite(backward, LOW);
 }
 
-void sensor() {
+void distance_sensor() {
   int duration, distance;
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(1000); //pauses the program for 1 millisecond
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin,HIGH);
   distance = (duration/2) / 29.1;
+  Serial.print("Distance: ");
   Serial.print(distance);
   Serial.print(" cm");
-  Serial.print('\n'); 
+  Serial.print('\n');
+  //ble_write(distance); 
 }
+
+void light_sensor(){
+  uint16_t light_sen_lux = LightSensor.GetLightIntensity();// Get Lux value
+  Serial.print("Light: ");
+  Serial.print(light_sen_lux);
+  Serial.print(" lux");
+ // ble_write(light_sen_lux);
+}
+
 void loop()
 {
   
@@ -115,6 +134,7 @@ void loop()
   
   ble_do_events();
   
-  sensor();
+  distance_sensor();
+  light_sensor();
 }
 
