9d07d0753dace2240fc7e50b484777ca5556bfee
martog <martin.holy.grigorov@gmail.com>
2016-01-08 22:21:52 +0200

    Accelerometer works!!!! (forward,backward,forward_right,backward_right,forward_left,backward_left)

--- app/src/main/java/com/example/cripz/bluetoothrccarcontroller/Accelerometer.java
+++ app/src/main/java/com/example/cripz/bluetoothrccarcontroller/Accelerometer.java
@@ -12,12 +12,13 @@ import android.widget.Toast;
 
 public class Accelerometer extends AppCompatActivity implements SensorEventListener {
     private float x, y, z;
+    private float x0, y0, z0;
     private TextView stateText;
     private String state = "state";
     private String lastState = "lastState";
-    // private float x0, y0, z0;
     private Sensor acc;
     private SensorManager senMng;
+    private int flag = 0;
 
 
     @Override
@@ -39,109 +40,114 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
 
     }
 
-    private String goForward(){
-        MainControl.sendMessage("f"); //go forward
-        MainControl.sendMessage("g"); //stop backward
-        MainControl.sendMessage("h"); //stop left
-        MainControl.sendMessage("j"); //stop right
-        lastState = "MOVING_FORWARD";
-
-        return lastState;
-    }
-
-    private String goBackward(){
-        MainControl.sendMessage("k"); //stop forward
-        MainControl.sendMessage("b"); //go backward
-        MainControl.sendMessage("h"); //stop left
-        MainControl.sendMessage("j"); //stop right
-        lastState = "MOVING_BACKWARD";
-
-        return lastState;
-    }
-
-    private String goRight(){
-        MainControl.sendMessage("k"); //stop forward
-        MainControl.sendMessage("g"); //stop backward
-        MainControl.sendMessage("h"); //stop left
-        MainControl.sendMessage("r"); //go right
-        lastState = "MOVING_RIGHT";
-
-        return lastState;
-    }
-
-    private String goLeft(){
-        MainControl.sendMessage("k"); //stop forward
-        MainControl.sendMessage("g"); //stop backward
-        MainControl.sendMessage("l"); //go left
-        MainControl.sendMessage("j"); //stop right
-        lastState = "MOVING_LEFT";
-
-        return lastState;
-    }
-
-
-    private String stop(){
+    private void stop(){
         MainControl.sendMessage("k"); //stop forward
         MainControl.sendMessage("g"); //stop backward
         MainControl.sendMessage("h"); //stop left
         MainControl.sendMessage("j"); //stop right
-        lastState = "STAY";
-
-        return lastState;
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+       /* if(flag == 0) {
+            x0 = Math.round(event.values[0]);
+            y0 = Math.round(event.values[1]);
+            z0 = Math.round(event.values[2]);
+            flag = 1;
+        }*/
+
         x = Math.round(event.values[0]);
         y = Math.round(event.values[1]);
         z = Math.round(event.values[2]);
 
         System.out.println("X: " + x + "   Y: " + y + "   Z: " + z);
         stateText = (TextView)findViewById(R.id.stateTextID);
-        //stateText.setText(("X: " + x + "   Y: " + y + "   Z: " + z + state));
 
+        if(z > 2){
 
-        if (y <= -3) {
+            if(y <= -1){
+
+                state = "MOVING_FORWARD_LEFT";
+                if(!lastState.equals(state)){
+                    stateText.setText(state);
+                    MainControl.sendMessage("f"); //go forward
+                    MainControl.sendMessage("g"); //stop backward
+                    MainControl.sendMessage("l"); //go left
+                    MainControl.sendMessage("j"); //stop right
+                    lastState = state;
+                }
+
+            }else if(y > 1){
+                state = "MOVING_FORWARD_RIGHT";
+                if(!lastState.equals(state)){
+                    stateText.setText(state);
+                    MainControl.sendMessage("f"); //go forward
+                    MainControl.sendMessage("g"); //stop backward
+                    MainControl.sendMessage("h"); //stop left
+                    MainControl.sendMessage("r"); //go right
+                    lastState = state;
+                }
+            }else{
+                state = "MOVING_FORWARD";
+                if(!lastState.equals(state)) {
+                    stateText.setText(state);
+                    MainControl.sendMessage("f"); //go forward
+                    MainControl.sendMessage("g"); //stop backward
+                    MainControl.sendMessage("h"); //stop left
+                    MainControl.sendMessage("j"); //stop right
+                    lastState = state;
+                }
+            }
+        }else if(z < 0){
+            if(y <= -1){
+
+                state = "MOVING_BACKWARD_LEFT";
+                if(!lastState.equals(state)){
+                    stateText.setText(state);
+                    MainControl.sendMessage("k"); //stop forward
+                    MainControl.sendMessage("b"); //go backward
+                    MainControl.sendMessage("l"); //go left
+                    MainControl.sendMessage("j"); //stop right
+                    lastState = state;
+                }
+
+            }else if(y > 1){
+                state = "MOVING_BACKWARD_RIGHT";
+                if(!lastState.equals(state)){
+                    stateText.setText(state);
+                    MainControl.sendMessage("k"); //stop forward
+                    MainControl.sendMessage("b"); //go backward
+                    MainControl.sendMessage("h"); //stop left
+                    MainControl.sendMessage("r"); //go right
+                    lastState = state;
+                }
+            }else {
+                state = "MOVING_BACKWARD";
+                if (!lastState.equals(state)) {
+                    stateText.setText(state);
+                    MainControl.sendMessage("k"); //stop forward
+                    MainControl.sendMessage("b"); //go backward
+                    MainControl.sendMessage("h"); //stop left
+                    MainControl.sendMessage("j"); //stop right
+                    lastState = state;
+                }
+            }
+        }else{
+            state = "STAY";
+            if(!lastState.equals(state)) {
+                stateText.setText(state);
+                stop();
+                lastState = state;
+            }
+        }
+
+       /* if (y <= -3) {
             state = "MOVING_LEFT";
             if(!lastState.equals(state)){
                 goLeft();
             }
             stateText.setText(state);
-        }
-
-        if (y >= 3) {
-            state = "MOVING_RIGHT";
-            if(!lastState.equals(state)){
-                goRight();
-            }
-            stateText.setText(state);
-        }
-
-        if(z >= 2){
-            state = "MOVING_FORWARD";
-            if(!lastState.equals(state)){
-                goForward();
-                stateText.setText(state);
-            }
-        }
-
-        if (z <= -2){
-            state = "MOVING_BACKWARD";
-            if(!lastState.equals(state)){
-                goBackward();
-            }
-            stateText.setText(state);
-        }
-
-
-        if ((y == -1 || y == 0 || y == 1 ) && (z == -1 || z == 0 || z == 1 )) {
-            state = "STAY";
-            if(!lastState.equals(state)){
-                stop();
-            }
-            stateText.setText(state);
-        }
-
+        }*/
 
     }
 
